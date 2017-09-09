cfac43b6da008ebe57787747a776d7c8a9935cb0
Sandeep Sekhon <sandysss79@gmail.com>
2017-09-09 06:04:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string.toLowerCase() === 'hello') {
-    return "I can\'t hear you!";
-  } else if (string.toUpperCase() === 'HELLO') {
+  if (string === 'hello') {
+    return "I can't hear you!";
+  } else if (string === 'HELLO') {
     return "YES INDEED!";
   } else if (string === "I love you, Grandma.") {
     return "I love you, too.";
